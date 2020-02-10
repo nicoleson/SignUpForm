7f983591ff5039ece868c60c0dee638df50582c8
nicoleson <nys4214@gmail.com>
2020-02-11 07:53:26 +1100

    Validation and tag helpers have added.
    - ModelState.IsValid
    - asp-validation-for

--- SignUpForm/Controllers/HomeController.cs
+++ SignUpForm/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SignUpForm.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,8 +12,26 @@ namespace SignUpForm.Controllers
     public class HomeController : Controller
     {
         // GET: /<controller>/
+
+        // Displays the Student.cshtml Razor view file.
         public IActionResult Student()
         {
+            return View();
+        }
+
+        // Receives values from html view with HttpPost
+        [HttpPost]
+        public IActionResult Student([Bind("Name, Age")] Student model)
+        {
+            // ModelState.IsValid checks if the input value of the model has an error or not.
+            // If the validation is true, it stores the model data into Student table.
+            if (ModelState.IsValid)
+            {
+            }
+            // If the validation is false, shows the error.
+            else
+            {
+            }
             return View();
         }
     }
---
+++ SignUpForm/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignUpForm.Models
+{
+    // Matching with an input property in View
+    public class Student
+    {
+        [Required, MaxLength(50)]
+        public string Name { get; set; }
+        [Range(15,70)]
+        public int Age { get; set; }
+        [Required, MinLength(5)]
+        public string Country { get; set; }
+    }
+}
